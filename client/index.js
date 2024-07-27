b3c624a52f13b6e0477a93dab3d02571acc2689a
rei <reimakesgames.dev@gmail.com>
2024-07-27 23:48:08 +0800

    change URI

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 const LOGIN_BUTTON = document.getElementById("login-button")
 
 LOGIN_BUTTON.addEventListener("click", async () => {
-	window.location.href = `https://discord.com/oauth2/authorize?client_id=1266028997381193738&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fredirect&scope=identify`
+	// window.location.href = `https://discord.com/oauth2/authorize?client_id=1266028997381193738&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fredirect&scope=identify`
+	window.location.href = `https://discord.com/oauth2/authorize?client_id=1266028997381193738&response_type=code&redirect_uri=https%3A%2F%2Fcherry.reicaffie.com%2Fapi%2Fauth%2Fredirect&scope=identify`
 })
